dfbc35048635b19c1c4c11d62622126145d5d25e
shafayet <shafayetsec09@gmail.com>
2024-11-27 17:33:32 +0600

    tailwind css install and implement

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,19 @@
+// ember-cli-build.js
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
+  let app = new EmberApp(defaults, {
+    // PostCSS options
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require('tailwindcss')('./tailwind.config.js'), // Ensure this points to the correct config
+          require('autoprefixer'),
+        ],
+      },
+    },
   });
 
   return app.toTree();
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './app/**/*.hbs', // Include all Handlebars templates
+    './app/**/*.js',  // Include all JavaScript files
+    './app/**/*.css', // Include all CSS files
+  ],
+  theme: {
+    extend: {}, // Customize the theme if needed
+  },
+  plugins: [],
+};
